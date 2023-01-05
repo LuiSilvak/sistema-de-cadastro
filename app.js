f33c12f73ecfa53dd86e680df8b0eb2ffd005210
Luís Silva <90908115+LuiSilvak@users.noreply.github.com>
2023-01-05 15:27:17 -0300

    Adicionando comandos no JavaScript

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 console.log("Rodando sistema de gerenciamento de jogos")
 
 console.log("Testando funcionalidade do GitHub")
+
+//Console log de teste
+
+console.log("Commit Teste")
+
+console.log("Enviando ao GITHUB")
